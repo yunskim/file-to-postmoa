a839ee8e0974c3a839370936a4185a30fea29e73
yunskim <yeonseung.kim@gmail.com>
2025-06-12 17:45:26 +0900

    menuBar, QAction 추가

--- main_window.py
+++ main_window.py
@@ -1,7 +1,7 @@
 import sys
 
-from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6.QtGui import QIcon, QAction
+from PyQt6.QtWidgets import QMainWindow, QApplication, QTableView
 
 
 class MainWindow(QMainWindow):
@@ -11,6 +11,19 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Hwp, Hwpx, Odt, Xls, Xlsx to Postmoa Converter")
         self.setWindowIcon(QIcon("icon.png"))
         self.setGeometry(300, 300, 600, 400)
+
+        # 파일에서 주소를 추출해 보여주는 table을 central widget으로 설정함
+        self.table_view = QTableView(self)
+        self.setCentralWidget(self.table_view)
+
+        # menu 추가
+        menu_bar = self.menuBar()
+
+        file_menu = menu_bar.addMenu("File")
+        file_open_action = QAction('Open File', self)
+        file_menu.addAction(file_open_action)
+
+        self.setMenuBar(menu_bar)
 
 
 if __name__ == '__main__':

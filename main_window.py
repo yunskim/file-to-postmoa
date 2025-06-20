e8a0e1f8f7c1cb27a7fa860eda058db08a99aa54
yunskim <yeonseung.kim@gmail.com>
2025-06-20 16:09:32 +0900

    창봉투 주소 출력을 추가함

--- main_window.py
+++ main_window.py
@@ -14,6 +14,20 @@ from typing import AnyStr
 from pypdf import PdfReader
 import arrow
 import win32com.client as win32
+import textwrap
+
+from reportlab.lib.pagesizes import A4, landscape
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.units import mm
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
+# print(A4)
+
+pdfmetrics.registerFont(TTFont("맑은고딕", "malgun.ttf"))
+A4_width, A4_height = A4
+A4_width_in_mm = int(A4_width / mm)
+A4_height_in_mm = int(A4_height / mm)
 
 FILTERS = [
     "Hwp (*.hwp *.hwpx *.odt )",
@@ -185,16 +199,19 @@ class MainWindow(QMainWindow):
                 datetime=arrow.now().format('YYYY-MM-DD HHmmss'))
             save_to_postmoa_registered_mail_path = directory / '{datetime}_등기우편.xls'.format(
                 datetime=arrow.now().format('YYYY-MM-DD HHmmss'))
+            save_to_save_to_windowed_envelop_pdf_path = directory / '{datetime}_창봉투_주소.pdf'.format(
+                datetime=arrow.now().format('YYYY-MM-DD HHmmss'))
 
-            self.save_to_postmoa_normal_mail(save_to_postmoa_normal_mail_path)
-            self.save_to_postmoa_registered_mail(save_to_postmoa_registered_mail_path)
+            self.save_to_postmoa_normal_mail_excel(save_to_postmoa_normal_mail_path)
+            self.save_to_postmoa_registered_mail_excel(save_to_postmoa_registered_mail_path)
+            self.save_to_windowed_envelop_pdf(save_to_save_to_windowed_envelop_pdf_path)
 
         except pywintypes.com_error as err:
             print(err)
             QMessageBox.critical(self, 'Error', 'table is not converted to postmoa')
             return
 
-    def save_to_postmoa_normal_mail(self, target: pathlib.Path | str):
+    def save_to_postmoa_normal_mail_excel(self, target: pathlib.Path | str):
         target = pathlib.Path(target)
         df_normal_mail = NORMAL_MAIL_EMPTY_DATAFRAME.copy(deep=True)
         # print(f'update 전 {df_normal_mail}')
@@ -215,38 +232,9 @@ class MainWindow(QMainWindow):
             df_normal_mail['통수*'] = '1'
 
             df_normal_mail.to_excel(target, index=False)
-            self.to_xls(target)
+            self.save_to_xls(target)
 
-    @staticmethod
-    def to_xls(xlsx: str | pathlib.Path) -> str:
-        """
-        df.to_excel()이 xlsx만 지원해서
-        일단 xlsx로 저장하고 xls로 다시 바꾸는 method를 작성함
-        
-        :param xlsx: 
-        :return: 
-        """
-        if isinstance(xlsx, str):
-            xlsx = pathlib.Path(xlsx)
-
-        excel_app = win32.gencache.EnsureDispatch('Excel.Application')
-        wb = excel_app.Workbooks.Open(xlsx)
-
-        xls = xlsx.with_suffix('.xls')
-        xls = str(xls)
-
-        # https://stackoverflow.com/questions/42182126/suppress-save-as-prompt
-        excel_app.DisplayAlerts = False
-
-        wb.SaveAs(xls, FileFormat=56)  # 56은 .xls
-        wb.Close()
-        # excel_app.Quit()
-
-        excel_app.DisplayAlerts = True
-
-        return xls
-
-    def save_to_postmoa_registered_mail(self, target: pathlib.Path | str):
+    def save_to_postmoa_registered_mail_excel(self, target: pathlib.Path | str):
         target = pathlib.Path(target)
         df_registered_mail = REGISTERED_MAIL_EMPTY_DATAFRAME.copy(deep=True)
 
@@ -267,7 +255,74 @@ class MainWindow(QMainWindow):
             df_registered_mail['환부*'] = '환부불능'
 
             df_registered_mail.to_excel(target, index=False)
-            self.to_xls(target)
+            self.save_to_xls(target)
+
+    def save_to_windowed_envelop_pdf(self, target: pathlib.Path | str):
+        print(f'save_to_windowed_envelop_pdf: {target}')
+
+        target = pathlib.Path(target)
+        windowed_envelop_pdf = Canvas(filename=str(target), pagesize=A4)
+
+        for row in self.data.itertuples():
+            index, name, zipcode, address, title, bike_number, due_date = row
+
+            # print(index, name, zipcode, address, title, bike_number, due_date)
+            self.draw_text_to_pdf(windowed_envelop_pdf, address, 85, 244, 30, 2, "맑은고딕", 10)
+            self.draw_text_to_pdf(windowed_envelop_pdf, name, 85, 230, 30, 2, "맑은고딕", 15)
+
+            self.draw_line_to_pdf(windowed_envelop_pdf, 0, 204, A4_width_in_mm, 204)
+            self.draw_line_to_pdf(windowed_envelop_pdf, 0, 110, A4_width_in_mm, 110)
+            self.draw_line_to_pdf(windowed_envelop_pdf, 0, 17, A4_width_in_mm, 17)
+
+            windowed_envelop_pdf.showPage()  # 한 페이지 완성
+
+        windowed_envelop_pdf.save()  # 전체 pdf 닫기
+
+    @staticmethod
+    def draw_text_to_pdf(canvas: Canvas,
+                         text: str,
+                         horizontal_offset: int,
+                         vertical_offset: int,
+                         max_text_length: int,
+                         row_gap: int,
+                         font: str,
+                         font_size: int):
+        """
+        
+        :param canvas: 추가할 pdf canvas object
+        :param text: 추가할 str
+        :param horizontal_offset: text box의 left coordinate(from left to right) in mm
+        :param vertical_offset: text box의 top coordinate(from bottom to top) in mm
+        :param max_text_length: 한 줄에 출력할 수 있는 글자 수
+        :param row_gap: 줄 사이 간격 in mm
+        :param font: pdfmetrics.registerFont로 추가된 폰트의 str
+        :param font_size: 폰트 크기 in pt
+        :return: 
+        """
+        canvas.setFont(font, font_size)
+        wrapped_text_rows = textwrap.wrap(text, max_text_length)
+
+        for i, row in enumerate(wrapped_text_rows):
+            row_horizontal_offset_in_pt = horizontal_offset * mm
+            row_horizontal_offset_in_pt -= font_size
+            row_vertical_offset_in_pt = (vertical_offset * mm) - (font_size + (row_gap * mm)) * i
+            row_vertical_offset_in_pt -= font_size
+
+            canvas.drawString(row_horizontal_offset_in_pt, row_vertical_offset_in_pt, row)
+
+    @staticmethod
+    def draw_line_to_pdf(canvas: Canvas,
+                         x1: int, y1: int, x2: int, y2: int):
+        """
+        (x1, y1)에서 (x2, y2)까지 line 그리기
+        :param canvas:
+        :param x1: in mm
+        :param y1: in mm
+        :param x2: in mm
+        :param y2: in mm
+        :return:
+        """
+        canvas.line(x1 * mm, y1 * mm, x2 * mm, y2 * mm)
 
     def open_file_dialog(self):
         files, filter_used = QFileDialog.getOpenFileNames(parent=self,
@@ -309,6 +364,35 @@ class MainWindow(QMainWindow):
             event.accept()
         else:
             event.ignore()
+
+    @staticmethod
+    def save_to_xls(xlsx: str | pathlib.Path) -> str:
+        """
+        df.to_excel()이 xlsx만 지원해서
+        일단 xlsx로 저장하고 xls로 다시 바꾸는 method를 작성함
+
+        :param xlsx:
+        :return:
+        """
+        if isinstance(xlsx, str):
+            xlsx = pathlib.Path(xlsx)
+
+        excel_app = win32.gencache.EnsureDispatch('Excel.Application')
+        wb = excel_app.Workbooks.Open(xlsx)
+
+        xls = xlsx.with_suffix('.xls')
+        xls = str(xls)
+
+        # https://stackoverflow.com/questions/42182126/suppress-save-as-prompt
+        excel_app.DisplayAlerts = False
+
+        wb.SaveAs(xls, FileFormat=56)  # 56은 .xls
+        wb.Close()
+        # excel_app.Quit()
+
+        excel_app.DisplayAlerts = True
+
+        return xls
 
     @staticmethod
     def extract_pattern_from_pdf(pdf: pathlib.Path | str, pattern: re.Pattern) -> tuple[AnyStr, ...]:
